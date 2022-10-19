9cb7afaa8cd3e632df136e488626a9f4f2ddd339
JoelRaimundo <joelf2000@hotmail.com.ar>
2022-10-18 22:26:49 -0300

    propiedades

--- src/app/public/pages/property-details/property-details.component.ts
+++ src/app/public/pages/property-details/property-details.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from 'src/app/services/storage.service';
 
@@ -10,14 +11,19 @@ export class PropertyDetailsComponent implements OnInit {
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   prop: any;
 
-  constructor(private storageSvc: StorageService) { }
+  constructor
+  (private storageSvc: StorageService, private location:Location) {   }
 
   getPropertyById(id: string) {
-    this.storageSvc.GetByParameter("properties", "id", id).subscribe((p) => { this.prop = p[0] })
+    this.storageSvc.GetByParameter("properties", "id", id).subscribe((p) => {
+      this.prop = p[0] 
+      console.log(p)
+    })
+  console.log(id)
   }
 
   ngOnInit(): void {
-    this.getPropertyById("1")
+    this.getPropertyById(this.location.path().split('/')[2])
   }
 
 
